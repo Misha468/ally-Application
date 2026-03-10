060733111eaf3e4ff6508eece2f5ad2e1c2e754a
ShartiShitCoder <mixa102006@ya.ru>
2026-03-11 01:18:49 +0300

    main page

--- app/(tabs)/_layout.tsx
+++ app/(tabs)/_layout.tsx
@@ -1,35 +1,158 @@
-import { Tabs } from 'expo-router';
-import React from 'react';
-
-import { HapticTab } from '@/components/haptic-tab';
-import { IconSymbol } from '@/components/ui/icon-symbol';
-import { Colors } from '@/constants/theme';
-import { useColorScheme } from '@/hooks/use-color-scheme';
-
+import React from "react";
+import {
+  Image,
+  ImageBackground,
+  StyleSheet,
+  Text,
+  TouchableOpacity,
+  View,
+} from "react-native";
 export default function TabLayout() {
-  const colorScheme = useColorScheme();
-
   return (
-    <Tabs
-      screenOptions={{
-        tabBarActiveTintColor: Colors[colorScheme ?? 'light'].tint,
-        headerShown: false,
-        tabBarButton: HapticTab,
-      }}>
-      <Tabs.Screen
-        name="index"
-        options={{
-          title: 'Home',
-          tabBarIcon: ({ color }) => <IconSymbol size={28} name="house.fill" color={color} />,
-        }}
-      />
-      <Tabs.Screen
-        name="explore"
-        options={{
-          title: 'Explore',
-          tabBarIcon: ({ color }) => <IconSymbol size={28} name="paperplane.fill" color={color} />,
-        }}
-      />
-    </Tabs>
+    <ImageBackground
+      style={styles.background}
+      source={require("../../assets/images/background.png")}
+    >
+      <View style={styles.header}>
+        <Text style={styles.companyName}>ALLY</Text>
+      </View>
+      <View style={styles.titleWrapper}>
+        <Text style={styles.textTitle}>Что будем творить?</Text>
+      </View>
+      <View style={styles.authentification}>
+        <View style={styles.authTopPart}>
+          <TouchableOpacity style={styles.signInButton}>
+            <Text style={styles.buttonText}>Войти</Text>
+          </TouchableOpacity>
+          <TouchableOpacity style={styles.iconWrapper}>
+            <Image
+              style={styles.icon}
+              source={require("../../assets/images/signInIcons/yandex.png")}
+            />
+          </TouchableOpacity>
+          <TouchableOpacity style={styles.iconWrapper}>
+            <Image
+              style={styles.bigIcon}
+              source={require("../../assets/images/signInIcons/google.png")}
+            />
+          </TouchableOpacity>
+          <TouchableOpacity style={styles.iconWrapper}>
+            <Image
+              style={styles.icon}
+              source={require("../../assets/images/signInIcons/gmail.png")}
+            />
+          </TouchableOpacity>
+        </View>
+        <Text style={styles.gappedText}>ИЛИ</Text>
+        <View>
+          <TouchableOpacity style={styles.signUpButton}>
+            <Text style={styles.buttonText}>Зарегистрироваться</Text>
+          </TouchableOpacity>
+        </View>
+      </View>
+    </ImageBackground>
   );
 }
+
+const styles = StyleSheet.create({
+  icon: {
+    width: 35,
+    height: 35,
+  },
+  bigIcon: {
+    width: 45,
+    height: 45,
+  },
+  background: {
+    width: "100%",
+    height: "100%",
+    display: "flex",
+  },
+  header: {
+    width: "100%",
+    height: 150,
+    display: "flex",
+    alignItems: "flex-end",
+    paddingRight: 25,
+    justifyContent: "center",
+  },
+  companyName: {
+    fontSize: 34,
+    color: "#e73f19",
+    fontFamily: "InstSansBold",
+  },
+  titleWrapper: {
+    width: "100%",
+    flexBasis: "auto",
+    flexGrow: 1,
+    flexShrink: 1,
+  },
+  textTitle: {
+    fontSize: 26,
+    letterSpacing: 1.5,
+    textTransform: "uppercase",
+    textAlign: "center",
+    color: "#212121",
+    fontFamily: "InstSansSemiBold",
+  },
+  authentification: {
+    width: "90%",
+    height: 200,
+    marginHorizontal: "auto",
+    borderTopLeftRadius: 15,
+    borderTopRightRadius: 15,
+    boxShadow: "0px 0px 30px rgba(0,0,0,0.2)",
+    backgroundColor: "#e6e6e3",
+    display: "flex",
+    gap: 15,
+    alignItems: "center",
+    justifyContent: "center",
+  },
+  buttonText: {
+    fontSize: 24,
+    fontFamily: "InstSansMed",
+  },
+  signInButton: {
+    backgroundColor: "#f0f0f0",
+    borderRadius: 25,
+    width: 150,
+    height: 45,
+    display: "flex",
+    alignItems: "center",
+    justifyContent: "center",
+  },
+  signUpButton: {
+    backgroundColor: "#f0f0f0",
+    borderRadius: 25,
+    width: 310,
+    height: 45,
+    display: "flex",
+    alignItems: "center",
+    justifyContent: "center",
+  },
+  gappedText: {
+    fontSize: 20,
+    fontFamily: "InstSansMed",
+  },
+  authTopPart: {
+    width: 310,
+    display: "flex",
+    flexDirection: "row",
+    alignItems: "center",
+    justifyContent: "space-between",
+  },
+  signInIcons: {
+    display: "flex",
+    flexDirection: "row",
+    gap: 5,
+  },
+  iconWrapper: {
+    width: 45,
+    height: 45,
+    borderRadius: "100%",
+    backgroundColor: "#f0f0f0",
+    display: "flex",
+    alignItems: "center",
+    justifyContent: "center",
+  },
+});
--- app/_layout.tsx
+++ app/_layout.tsx
@@ -1,24 +1,25 @@
-import { DarkTheme, DefaultTheme, ThemeProvider } from '@react-navigation/native';
-import { Stack } from 'expo-router';
-import { StatusBar } from 'expo-status-bar';
-import 'react-native-reanimated';
-
-import { useColorScheme } from '@/hooks/use-color-scheme';
-
-export const unstable_settings = {
-  anchor: '(tabs)',
-};
-
+import { useFonts } from "expo-font";
+import { Stack } from "expo-router";
+import { useEffect } from "react";
+import { StyleSheet } from "react-native";
 export default function RootLayout() {
-  const colorScheme = useColorScheme();
-
+  const [loaded] = useFonts({
+    InstSansMed: require("../assets/fonts/InstrumentSans-Medium.ttf"),
+    InstSansSemiBold: require("../assets/fonts/InstrumentSans-SemiBold.ttf"),
+    InstSansBold: require("../assets/fonts/InstrumentSans-Bold.ttf"),
+  });
+  useEffect(() => {
+    if (loaded) {
+    }
+  });
   return (
-    <ThemeProvider value={colorScheme === 'dark' ? DarkTheme : DefaultTheme}>
-      <Stack>
-        <Stack.Screen name="(tabs)" options={{ headerShown: false }} />
-        <Stack.Screen name="modal" options={{ presentation: 'modal', title: 'Modal' }} />
-      </Stack>
-      <StatusBar style="auto" />
-    </ThemeProvider>
+    <Stack>
+      <Stack.Screen name="(tabs)" options={{ headerShown: false }} />
+    </Stack>
   );
 }
+const styles = StyleSheet.create({
+  background: {
+    backgroundColor: "#e6e6e3",
+  },
+});
